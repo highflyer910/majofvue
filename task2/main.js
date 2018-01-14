1c6be791e992d83ee3a2dfaf594199657d3f2d83
Thea <tmush2014@agruni.edu.ge>
2018-01-14 20:49:14 +0400

    task2

---
+++ task2/main.js
@@ -0,0 +1,12 @@
+new Vue({
+	el: ".container",
+	data: {
+		story: {
+			name: "Thea",
+			weight: "52",
+			height: "168",
+			eyeColor: "brown",
+            favoriteFood: "Mchadi with cheese"  
+		},
+	}
+});
