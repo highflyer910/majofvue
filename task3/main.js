b2e824a1a6063b5aebae41e76428b6c0049913b9
Thea <tmush2014@agruni.edu.ge>
2018-01-21 00:04:01 +0400

    Task3

--- task3/main.js
+++ task3/main.js
@@ -7,9 +7,24 @@ new Vue({
       {name: "Mr.Red", votes: 50}
    ]
   },
-  computed: {
-   
-      }
-    }
-  },
-});
+   computed: {
+            mayor: function () {
+              
+                var candidatesSorted = this.candidates.sort(function (a, b) {
+                    return b.votes - a.votes;
+                });
+              
+                return candidatesSorted[0];
+            }
+        },
+        methods: {
+          
+            clear: function () {
+               
+                this.candidates = this.candidates.map(function (candidate) {
+                    candidate.votes = 0;
+                    return candidate;
+                })
+            }
+        }
+    });
