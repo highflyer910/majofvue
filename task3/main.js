380162b2fa4a1a238f8970d860346ffbaa95d780
Thea <tmush2014@agruni.edu.ge>
2018-01-20 23:40:20 +0400

    Task3

---
+++ task3/main.js
@@ -0,0 +1,15 @@
+new Vue({
+  el: '.container',
+  data: {
+   candidates: [
+      {name: "Mr.Black", votes: 140},
+      {name: "Mr.White", votes: 20},
+      {name: "Mr.Red", votes: 50}
+   ]
+  },
+  computed: {
+   
+      }
+    }
+  },
+});
